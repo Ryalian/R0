2d90e6a73e66b18bc4716eda153c29769d22b42c
Ryan Chan <ryalianese@gmail.com>
2016-10-08 14:46:31 -0700

    first step of the project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "public");
+var SRC_DIR = path.resolve(__dirname, "appSrc");
+
+var config = {
+    entry: [
+        SRC_DIR + "/app/index.js",
+        SRC_DIR + "/style/main.less",
+    ],
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [
+            // ES6 & up
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            },
+            // LESS
+            {
+                test: /\.less$/,
+                loader: "style!css!autoprefixer!less"
+            },
+        ]
+    },
+    watch: true
+};
+
+module.exports = config;
