dec11ae76edbdc2da7264bba93efddaa71ab0b2a
Ryalian <ryalianese@gmail.com>
2016-10-24 18:33:19 -0700

    Finding way to make images work

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname + '/public'));
-app.get('/images', express.static(__dirname + '/images'));
 
 
 app.get('/', function (req, res) {
