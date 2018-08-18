fb2b35dd5eec1d28be3d56613886325c75ec6054
Ryalian <ryalianese@gmail.com>
2018-08-17 22:22:59 -0700

    more on picker and trying to play with node_env

--- appSrc/app/container/entryPlugs/calendar/DatePicker.jsx
+++ appSrc/app/container/entryPlugs/calendar/DatePicker.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { startOfMonth } from "date-fns";
+import { startOfMonth, addMonths } from "date-fns";
+import { getFormattedMonth } from "../../../util";
 
 import MonthItem from './MonthItem';
 
@@ -7,19 +8,33 @@ export default class DatePicker extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleSelect = this.handleSelect.bind(this);
         this.state = {
             month: startOfMonth(this.props.selectedDay)
         }
+
+        this.handleSelect = this.handleSelect.bind(this);
+        this.changeMonth = this.changeMonth.bind(this);
+        
     }
 
     handleSelect(date) {
         this.props.onSelect(date);
     }
 
+    changeMonth(diff) {
+        this.setState({
+            month: addMonths(this.state.month, diff)
+        })
+    }
+
     render() {
         return (
             <div>
+                <div>
+                    <button onClick={() => this.changeMonth(-1)}>{'<'}</button>
+                    {getFormattedMonth(this.state.month)}
+                    <button onClick={() => this.changeMonth(1)}>{'>'}</button>
+                </div>
                 <MonthItem
                     {...this.state}
                     selectedDay={this.props.selectedDay}
